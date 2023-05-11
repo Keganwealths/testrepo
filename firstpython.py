177486961104ca91b2d667998e68e6931c449bc2
Kevin Kegan <kevin.olome.16@gmail.com>
2023-05-11 16:15:59 +0300

    Update firstpython.py
    debug some errors

--- firstpython.py
+++ firstpython.py
@@ -1,6 +1,7 @@
-print('testing github')
+print('Welcome to the git Test \n==========================')
+print('This is a program to determine if the year you were born is a leap year')
 year=input(int('Enter the year to be determined if it is leap year or not: '))
-if (year%100!=0 AND Year%4==0) OR year%400==0:
+if ( year%100!=0  and  year%4==0) or year%400==0 :
   print(year,' Is a leap year')
 else:
   print('This is not a Leap year')
