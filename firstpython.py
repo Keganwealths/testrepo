349e7697af231538cacd2184f04759dc5c3a5e71
Kevin Kegan <kevin.olome.16@gmail.com>
2023-05-11 15:33:31 +0300

    Update firstpython.py
    Added a year determinant code

--- firstpython.py
+++ firstpython.py
@@ -1,1 +1,6 @@
 print('testing github')
+year=input(int('Enter the year to be determined if it is leap year or not: '))
+if (year%100!=0 AND Year%4==0) OR year%400==0:
+  print(year,' Is a leap year')
+else:
+  print('This is not a Leap year')
